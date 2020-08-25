29b53f5442a9811cd2f117ab68e33b664c6c3e76
Alex Bannow <rbannal@gmail.com>
2020-08-25 15:20:48 -0400

    added server url

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,40 +1,40 @@
 import React, { useState } from "react";
 import "./Search.css";
 
-const Search = props => {
+const Search = (props) => {
   const [search, setSearch] = useState("");
   const [error, setError] = useState("");
 
   //Handles errors. Mainly prevents a user from inputting an empty string.
   //Unrenders any error message by removing it from state
-  const handleChange = e => {
+  const handleChange = (e) => {
     if (error !== "") setError("");
     setSearch(e.target.value);
   };
 
   //Main fetch call. Grabs books data and sends it back to Main, where it gets passed to Results and
   //Processed through the children components
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let url = `http://localhost:8000/api/books/${search}`;
+    let url = `https://books10.herokuapp.com/api/books/${search}`;
     fetch(url, {
       method: "GET",
       headers: {
-        "content-type": "application/json"
-      }
+        "content-type": "application/json",
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
           setError(res.error);
           throw new Error(res.status);
         }
         return res.json();
       })
-      .then(res => {
+      .then((res) => {
         props.handleResults(res, search);
         setSearch("");
       })
-      .catch(error => {
+      .catch((error) => {
         //Displays any further errors
         console.log(error);
         setError(error);
@@ -42,7 +42,7 @@ const Search = props => {
   };
 
   //sets error when user tries to input a blank item
-  const handleEmptySubmit = e => {
+  const handleEmptySubmit = (e) => {
     e.preventDefault();
     setError("Please Enter A Search Query");
   };
@@ -52,7 +52,7 @@ const Search = props => {
       <form className="search_form">
         <input
           className="search_input"
-          onChange={e => handleChange(e)}
+          onChange={(e) => handleChange(e)}
           type="text"
           placeholder="Enter Search Terms..."
           value={search}
@@ -65,7 +65,7 @@ const Search = props => {
             <button
               className="search_button"
               type="submit"
-              onClick={e => handleEmptySubmit(e)}
+              onClick={(e) => handleEmptySubmit(e)}
             >
               Submit
             </button>
@@ -73,12 +73,15 @@ const Search = props => {
             <button
               className="search_button"
               type="submit"
-              onClick={e => handleSubmit(e)}
+              onClick={(e) => handleSubmit(e)}
             >
               Submit
             </button>
           )}
-          <button className="search_button" onClick={e => props.handleReset(e)}>
+          <button
+            className="search_button"
+            onClick={(e) => props.handleReset(e)}
+          >
             Clear Results
           </button>
         </div>
